421f3bcaafb544109387f0c374f06dcf567e79ad
redflame <vanya133719@gmail.com>
2021-04-02 19:35:28 +0300

    facebook auth done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'application#index'
+  get 'home/index'
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  root 'home#index'
   get 'signup' => 'users#new'
+  get 'home' => 'home#index'
 end
